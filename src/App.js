adb227b724c9fda5f449aca565bfea6366ce2d2b
alainagossett <91034239+alainagossett@users.noreply.github.com>
2021-12-22 17:47:43 -0700

    adds react functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 // IMPORT useState
-import React from 'react';
-import './styles.css';
+import { useState } from "react";
+import React from "react";
+import "./styles.css";
+import imagesArr from "./imageData";
 // IMPORT imageData.js AND STORE IT IN A VARIABLE CALLED imagesArr
 
 export default function App() {
-  // USE useState TO CREATE  [bigImage, setBigImage] 
+  // console.log(imagesArr[0].img);
+  const [bigImage, setBigImage] = useState(imagesArr[0].img);
+  // USE useState TO CREATE  [bigImage, setBigImage]
   // AND SET IT TO THE IMAGE URL OF THE FIRST ELEMENT IN THE ARRAY
 
-  // CREATE A HANDLE CLICK FUNCTION THAT ACCEPTS AN IMAGE URL
-  // THE FUNCTION SHOULD CALL setBigImage AND PASS IT THE URL
+  const handleClick = (url) => {
+    // CREATE A HANDLE CLICK FUNCTION THAT ACCEPTS AN IMAGE URL
+    // THE FUNCTION SHOULD CALL setBigImage AND PASS IT THE URL
+    setBigImage(url);
+  };
 
-  // CREATE A VARIABLE CALLED images THAT LOOPs OVER THE imagesArr AND RETURNS AN <IMG> ELEMENT
+  const images = imagesArr.map((image, index) => {
+    // CREATE A VARIABLE CALLED images THAT LOOPs OVER THE imagesArr AND RETURNS AN <IMG> ELEMENT
+    return (
+      <img
+        className="thumb"
+        src={image.img}
+        alt={image.city}
+        key={index}
+        onClick={() => handleClick(image.img)}
+      />
+    );
+  });
+  // console.log(images);
+
   // ASSIGN ALL OF THE PROPERTIES THAT IT NEEDS: src, alt, className, key INCLUDING AN onClick EVENT THAT CALLS THE HANDLE EVENT FUNCTION AND PASSES IT THE IMG URL
 
   return (
     <div className="App">
       <h1>Cities Of The World</h1>
       <div id="wrapper">
-        <div id="thumbnails">
-          {/* RENDER THE IMAGES ARRAY  */}
-        </div>
+        <div id="thumbnails">{images}</div>
+        <img
+          id="bigimage"
+          src={bigImage}
+          alt="city"
+          images={images}
+          handleClick={handleClick}
+        />
         {/* THE SRC IMAGE URL SHOULD BE SET TO THE VALUE THAT IS STORED IN bigImage */}
         {/* <img src="" id="bigimage" alt='bigImage'/> */}
       </div>
